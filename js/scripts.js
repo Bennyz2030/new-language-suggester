654d93d42157da56163645378211268776d10a6e
Bennyz2030 <nicholasbenzinger@gmail.com>
2022-03-25 11:46:58 -0700

    change first question and options

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     event.preventDefault();
-    const frontBack = parseInt($("input#front-or-back").val());
+    const favMeal = parseInt($("input#fav-meal").val());
     const book = parseInt($("input#book-genre").val());
     const streaming = parseInt($("input#streaming").val());
     const movie = parseInt($("input#fav-movie").val());
     const food = parseInt($("input#food").val());
     const wierdQuestions = parseInt($("input#wierd-questions").val());
+    const questionInput = (favMeal + book + streaming + movie + food + wierdQuestions);
+
+    if (questionInput)
   })
 })
